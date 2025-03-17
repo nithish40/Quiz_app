3eff1f9ecde24b9e66edb8ae66ce0036440ef686
nithish40 <nithishreddy2112@gmail.com>
2025-03-17 15:41:46 +0530

    updated

--- Front-end/src/components/navbar.jsx
+++ Front-end/src/components/navbar.jsx
@@ -84,7 +84,7 @@ export default function Example() {
                     href="#"
                     className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
                   >
-                    Your Profile
+                    Your profile
                   </a>
                 </MenuItem>
                 <MenuItem>
